0c21a166d01bf06beb3d4fc3bf430b0713442f07
Rashid <rashideram2@gmail.com>
2025-01-04 14:06:51 +0600

    added-admin-panel

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,16 +3,16 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
@@ -64,5 +64,9 @@ class User extends Authenticatable
     public function applicants(): HasMany
     {
         return $this->hasMany(Applicant::class, 'user_id');
+    }
+    public function canAccessPanel(Panel|\Filament\Panel $panel): bool
+    {
+        return str_ends_with($this->email, '@admin.com');
     }
 }
